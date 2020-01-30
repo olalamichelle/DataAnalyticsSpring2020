52316da1ca6d419af91652bcc24ffefc067d6aae
olalamichelle <32348847+olalamichelle@users.noreply.github.com>
2020-01-30 11:43:02 -0500

    Add files via upload

---
+++ lab1_part2.r
@@ -0,0 +1,62 @@
+# lab1 - part2
+# https://www.rdocumentation.org/packages/EnvStats/versions/2.3.1/topics/qqPlot
+
+# creating plots
+# ch 2 -- R Graphics Cookbook
+plot(mtcars$wt, mtcars$mpg)
+library(ggplot2)
+qplot(mtcars$wt, mtcasrs$mpg)
+qplot(wt, mpg, data = mtcars)
+ggplot(mtcars, aes(x-wt, y = mpg)) + geom_point()
+plot(pressure$temperature, pressure$pressure, type = "l")
+points(presure$temperature, presure$pressue)
+
+lines(pressure$temperature, pressure$temperature/2, col = "red")
+points(pressure$temperature, pressure$pressure/2, col = "blue")
+library(ggplot2)
+
+qplot(pressure$temperature, pressure$pressurem, geom = "line")
+qplot(pressure$temperature, pressure$pressurem, geom = "line")
+qplot(pressure, aes(x = temperature, y = pressure) + geom_line() + geom_point() )
+qplot(pressure, aes(x = temperature, y = pressure) + geom_line() + geom_point() )
+
+# creting bar graphs
+barpot*BOD$demand, names.arg = BOD$Time0
+table(mtcars$cyl)
+barplot(table(mtcars$cyl)) #generate a table of counts
+qplot(mtcars$cyl)
+qplot(factor(mtcars$cyl)) # treat cyl as discrete
+
+# bar graph of counts
+qplot(factor(cyl), data = mtcars)
+ggplot(mtcars, aex(x = factor(cyl))) + geom_bar()
+
+# create histogram
+# view the dirtibution of one =-dimentional data with a histogram
+hist(mtcars$mpg)
+hist(mtcars$mpg, break = 10)
+hist(mtcars$mpg, break = 5)
+hist(mtcars$mpg, break = 12)
+qplot(mgp, data = mtcars, binwidth = 4)
+ggplot(mtcars, aes(x = mgp)) + geom_histogrram(binwidth = 4)
+ggplot(mtcars, aes(x = mgp)) + geom_histogrram(binwidth = 5)
+
+
+# creating box-plot
+
+# pass it a factor of x values and a vector of y values
+plot(ToothGrowth$supp, ToothGrowth$len) 
+
+#formula syntax
+# if the two 
+boxplot(len ~ supp, data = ToothGrowth)
+
+
+
+
+      
+      
+      
+      
+      
+      )
